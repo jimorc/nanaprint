6d4ae6bb8c4f933d9b76ed80fc93ef1583a3e194
Jim Orcheson <jimorcheson@gmail.com>
2020-10-23 07:21:33 -0400

    Add simple project structure with Hello World prog

---
+++ apps/printerproperties/printerproperties.cpp
@@ -0,0 +1,23 @@
+/*
+ *	A program for using and testing printer related librarites in
+ *  the nanaprint C++ library. The libraries that are tested are OS 
+ *  or print system specific (e.g. Win32 or CUPS).  
+ *	Copyright(C) 2020 Jim Orcheson (jimorcheson@gmail.com)
+ *
+ *	Distributed under the Boost Software License, Version 1.0.
+ *	(See accompanying file LICENSE or copy at
+ *	http://www.boost.org/LICENSE_1_0.txt)
+ *
+ *	@file: apps/printerproperties/printerproperties.cpp
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
