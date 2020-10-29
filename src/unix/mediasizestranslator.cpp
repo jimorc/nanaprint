af3db81929ba6bd102977d5c17d2583337d3dbc5
Jim Orcheson <jimorcheson@gmail.com>
2020-10-29 16:05:22 -0400

    Create translated mediasizes for all paper sizes retrieved from Brother MFC-J470DW printer.

--- src/unix/mediasizestranslator.cpp
+++ src/unix/mediasizestranslator.cpp
@@ -3,12 +3,40 @@
 namespace nanaprint
 {
     MediaSizesTranslator::MediaSizesTranslator()
-        : m_mediaMap {
-            { "oe_photo-l_3.5x5in", "3.5x5in" },
-            { "na_index_4x6in", "4x6in" }
-        }
     {
-
+        m_mediaMap["oe_photo-l_3.5x5in"] = "3.5x5in";
+        m_mediaMap["oe_photo_l_3.5x5in_borderless"] = "3.5x5in";
+        m_mediaMap["na_monarch_3.875x7.5in"] = "Monarch 3.875x7.5in";
+        m_mediaMap["na_monarch_3.875x7.5in"] = "Monarch 3.875x7.5in";
+        m_mediaMap["na_index_4x6in"] = "4x6in";
+        m_mediaMap["na_index_4x6in_borderless"] = "4x6in";
+        m_mediaMap["na_number-10_4.125x9.5in"] = "No. 10 Envelope";
+        m_mediaMap["na_number-10_4.125x9.5in_borderless"] = "No. 10 Envelope";
+        m_mediaMap["iso_a6_105x148mm"] = "A6";
+        m_mediaMap["iso_a6_105x148mm_borderless"] = "A6";
+        m_mediaMap["iso_dl_110x220"] = "DL Envelope";
+        m_mediaMap["iso_dl_110x220_borderless"] = "DL Envelope";
+        m_mediaMap["na_5x7_5x7in"] = "5x7in";
+        m_mediaMap["na_5x7_5x7in_borderless"] = "5x7in";
+        m_mediaMap["na_index-5x8_5x8in"] = "5x8in";
+        m_mediaMap["na_index-5x8_5x8_borderless"] = "5x8in";
+        m_mediaMap["iso_a5_148x210mm"] = "A5";
+        m_mediaMap["iso_a5_148x210mm_borderless"] = "A5";
+        m_mediaMap["iso_c5_162x229mm"] = "C5";
+        m_mediaMap["iso_c5_162x229mm_borderless"] = "C5";
+        m_mediaMap["na_executive_7.25x10.5in"] = "Executive 7.25x10.5in";
+        m_mediaMap["na_executive_7.25x10.5in_borderless"] = "Executive 7.25x10.5in";
+        m_mediaMap["iso_a4_210x297mm"] = "A4";
+        m_mediaMap["iso_a4_210x297mm+borderless"] = "A4";
+        m_mediaMap["na_letter_x.5x11in"] = "Letter";
+        m_mediaMap["na_letter_x.5x11in_borderless"] = "Letter";
+        m_mediaMap["na_legal_8,5x14in"] = "Legal";
+        m_mediaMap["na_legal_8,5x14in_borderless"] = "Legal";
+        m_mediaMap["jpn_chou4_90x205mm"] = "Nagagata 4";
+        m_mediaMap["jpn_hagaki_100x148mm"] = "Hagaki";
+        m_mediaMap["jpn_you4_105x235mm"] = "Yougata 4";
+        m_mediaMap["jpn_chou3_120x235mm"] = "Nagagata 3";
+        m_mediaMap["jpn_oufuku_148x200mm"] = "Oufuku";
     }
 
     MediaSizesTranslator::~MediaSizesTranslator()
