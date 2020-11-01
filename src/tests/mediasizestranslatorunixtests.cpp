e7849992c950775f8e3770c656e5c395af464b73
Jim Orcheson <jimorcheson@gmail.com>
2020-11-01 07:53:21 -0500

    Combine multiple tests into one. Was the same test with different size names..

--- src/tests/mediasizestranslatorunixtests.cpp
+++ src/tests/mediasizestranslatorunixtests.cpp
@@ -6,18 +6,11 @@ using namespace nanaprint;
 using namespace std;
 
 // Test getTranslatedSize for oe_photo-l_3.5x5in
-TEST(MediaSizesTranslatorUnixTests, testMediaSizesTranslator3_5x5)
+TEST(MediaSizesTranslatorUnixTests, testMediaSizesTranslatorMatch)
 {
     MediaSizesTranslator translator;
-    string translatedSize = translator.getTranslatedSize("oe_photo-l_3.5x5in");
-    ASSERT_STREQ("3.5x5in", translatedSize.c_str());
-}
-
-// Test getTranslatedSize for na_index_4x6in
-TEST(MediaSizesTranslatorUnixTests, testMediaSizesTranslator4x6) {
-    MediaSizesTranslator translator;
-    string translatedSize = translator.getTranslatedSize("na_index-4x6_4x6in");
-    ASSERT_STREQ("4x6in", translatedSize.c_str());
+    ASSERT_STREQ("3.5x5in", translator.getTranslatedSize("oe_photo-l_3.5x5in").c_str());
+    ASSERT_STREQ("4x6in", translator.getTranslatedSize("na_index-4x6_4x6in").c_str());
 }
 
 // Test getTranslatedSize for media name that does not have a translated name
