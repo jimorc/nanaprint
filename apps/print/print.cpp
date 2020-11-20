095e20b5d0a4dd004cf1fcc4a6f4a3c64c456efc
Jim Orcheson <jimorcheson@gmail.com>
2020-11-20 06:57:26 -0500

    Add simple nana-based program that will be used to test printing. Main frame has a simple menu whose items currently display a message box when either is selected.

---
+++ apps/print/print.cpp
@@ -0,0 +1,49 @@
+/*
+ *	A program for using and testing 
+ *  the nanaprint C++ library. The libraries that are tested are OS 
+ *  or print system specific (e.g. Win32 or CUPS).  
+ *	Copyright(C) 2020 Jim Orcheson (jimorcheson@gmail.com)
+ *
+ *	Distributed under the Boost Software License, Version 1.0.
+ *	(See accompanying file LICENSE or copy at
+ *	http://www.boost.org/LICENSE_1_0.txt)
+ *
+ *	@file: apps/printmain/printmain.cpp
+ */
+
+#include <nana/gui.hpp>
+#include <nana/gui/widgets/menubar.hpp>
+#include <nana/gui/widgets/menu.hpp>
+
+using namespace nana;
+using namespace std;
+
+
+int main()
+{
+    form mainForm;
+    menubar mainMenu(mainForm);
+
+    mainMenu.events().mouse_up([](const arg_mouse& arg) {
+        if(mouse::left_button != arg.button)
+        return;
+    });
+
+    mainMenu.push_back("&File");
+    auto& fileMenu = mainMenu.at(0);
+    fileMenu.append("Page Setup...", [](menu::item_proxy& ip) {
+        msgbox msg("Page Setup clicked");
+        msg << ip.text();
+        msg.show();
+    });
+
+    fileMenu.append("Print...", [](menu::item_proxy& ip) {
+        msgbox msg("Print clicked");
+        msg << ip.text();
+        msg.show();
+    });
+
+    mainForm.show();
+    exec();
+    return 0;
+}
