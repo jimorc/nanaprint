26dc70e05f7955ce62a75c0a3c689f4777e12bad
Jim Orcheson <jimorcheson@gmail.com>
2021-02-12 11:27:09 -0500

    Add const_iterator struct to nanaprint_values class. Remove const_iterator struct from color_modes class.

--- include/values.h
+++ include/values.h
@@ -53,6 +53,38 @@ namespace nanaprint
                     pointer m_ptr;
             };
 
+            struct const_iterator
+            {
+                using iterator_category = std::random_access_iterator_tag;
+                using difference_type   = std::ptrdiff_t;
+                using value_type        = const T;
+                using pointer           = const T*;  // or also value_type*
+                using reference         = const T&;  // or also value_type& 
+
+                explicit const_iterator(pointer ptr) : m_ptr(ptr) {}
+                reference operator*() const { return *m_ptr; }
+                pointer operator->() const { return m_ptr; }
+
+                // Prefix increment
+                const_iterator& operator++() { m_ptr++; return *this; }  
+
+                // Postfix increment
+                const_iterator operator++(int) { const_iterator tmp = *this; ++(*this); return tmp; }
+
+                // Prefix decrement
+                const_iterator& operator--() { m_ptr--; return *this; }  
+
+                // Postfix decrement
+                const_iterator operator--(int) { const_iterator tmp = *this; --(*this); return tmp; }
+
+                friend bool operator== (const const_iterator& a, const const_iterator& b) { return a.m_ptr == b.m_ptr; };
+                friend bool operator!= (const const_iterator& a, const const_iterator& b) { return a.m_ptr != b.m_ptr; };
+                friend size_t operator- (const const_iterator&a, const const_iterator&b) { return a.m_ptr - b.m_ptr; };
+
+                private:
+                    pointer m_ptr;
+            };
+
             nanaprint_values() {}
             virtual ~nanaprint_values() {}
     };
